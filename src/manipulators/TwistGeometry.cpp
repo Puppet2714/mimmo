23d31db19f33de5e59dde93a71c9629b91fdd2fb
roccoarpa <rocco.arpa@optimad.it>
2017-08-04 13:31:01 +0200

    manipulators: update TwistGeometry to new PortManager

--- src/manipulators/TwistGeometry.cpp
+++ src/manipulators/TwistGeometry.cpp
@@ -89,15 +89,15 @@ TwistGeometry & TwistGeometry::operator=(const TwistGeometry & other){
 void
 TwistGeometry::buildPorts(){
     bool built = true;
-    built = (built && createPortIn<darray3E, TwistGeometry>(&m_origin, PortType::M_POINT, mimmo::pin::containerTAG::ARRAY3, mimmo::pin::dataTAG::FLOAT));
-    built = (built && createPortIn<darray3E, TwistGeometry>(&m_direction, PortType::M_AXIS, mimmo::pin::containerTAG::ARRAY3, mimmo::pin::dataTAG::FLOAT));
-    built = (built && createPortIn<double, TwistGeometry>(&m_alpha, PortType::M_VALUED, mimmo::pin::containerTAG::SCALAR, mimmo::pin::dataTAG::FLOAT));
-    built = (built && createPortIn<double, TwistGeometry>(&m_distance, PortType::M_VALUED2, mimmo::pin::containerTAG::SCALAR, mimmo::pin::dataTAG::FLOAT));
-    built = (built && createPortIn<dvector1D, TwistGeometry>(this, &mimmo::TwistGeometry::setFilter, PortType::M_FILTER, mimmo::pin::containerTAG::VECTOR, mimmo::pin::dataTAG::FLOAT));
-    built = (built && createPortIn<MimmoObject*, TwistGeometry>(&m_geometry, PortType::M_GEOM, mimmo::pin::containerTAG::SCALAR, mimmo::pin::dataTAG::MIMMO_, true));
-    built = (built && createPortOut<dvecarr3E, TwistGeometry>(this, &mimmo::TwistGeometry::getDisplacements, PortType::M_GDISPLS, mimmo::pin::containerTAG::VECARR3, mimmo::pin::dataTAG::FLOAT));
-    built = (built && createPortOut<std::pair<MimmoObject*, dvecarr3E*> , TwistGeometry>(this, &mimmo::TwistGeometry::getDeformedField, PortType::M_PAIRVECFIELD, mimmo::pin::containerTAG::PAIR, mimmo::pin::dataTAG::MIMMO_VECARR3FLOAT_));
-    built = (built && createPortOut<MimmoObject*, TwistGeometry>(this, &BaseManipulation::getGeometry, PortType::M_GEOM, mimmo::pin::containerTAG::SCALAR, mimmo::pin::dataTAG::MIMMO_));
+    built = (built && createPortIn<darray3E, TwistGeometry>(&m_origin, M_POINT));
+    built = (built && createPortIn<darray3E, TwistGeometry>(&m_direction, M_AXIS));
+    built = (built && createPortIn<double, TwistGeometry>(&m_alpha, M_VALUED));
+    built = (built && createPortIn<double, TwistGeometry>(&m_distance, M_VALUED2));
+    built = (built && createPortIn<dvector1D, TwistGeometry>(this, &mimmo::TwistGeometry::setFilter, M_FILTER));
+    built = (built && createPortIn<MimmoObject*, TwistGeometry>(&m_geometry, M_GEOM, true));
+    built = (built && createPortOut<dvecarr3E, TwistGeometry>(this, &mimmo::TwistGeometry::getDisplacements, M_GDISPLS));
+    built = (built && createPortOut<std::pair<MimmoObject*, dvecarr3E*> , TwistGeometry>(this, &mimmo::TwistGeometry::getDeformedField, M_PAIRVECFIELD));
+    built = (built && createPortOut<MimmoObject*, TwistGeometry>(this, &BaseManipulation::getGeometry, M_GEOM));
     m_arePortsBuilt = built;
 };
 
